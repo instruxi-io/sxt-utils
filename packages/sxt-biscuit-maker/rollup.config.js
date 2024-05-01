46a6047088f347ab1225643283001d815926b43c
Austin Przybysz <austin@instruxi.io>
2024-05-02 05:27:20 +0530

    Fix issue wasm lib imports

--- packages/sxt-biscuit-maker/rollup.config.js
+++ packages/sxt-biscuit-maker/rollup.config.js
@@ -1,34 +1,16 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
 import { defineConfig } from 'rollup';
-import autoExternal from 'rollup-plugin-auto-external';
 import dts from 'rollup-plugin-dts';
+import nodeResolve from "@rollup/plugin-node-resolve";
 const dir = 'distribution';
-const pkg = JSON.parse(readFileSync(join(process.cwd(), 'package.json'), 'utf8'));
-const input = pkg.exports
-	? Object.assign(
-			{},
-			...Object.entries(pkg.exports)
-				.filter(([key]) => !key.includes('.css'))
-				.map(([key, value]) => {
-					key = key === '.' ? 'index' : key.replace('./', '');
-					return [key, value.import || value.default || value];
-				})
-				.map((x) => ({ [x[0]]: x[1] }))
-	  )
-	: { ['index']: pkg.source };
-
 
 
 export default defineConfig([
 	{
-		input,
+		input: "src/index.js",
 		output: [{ dir: `${dir}`, format: 'esm' }],
 		plugins: [
-			autoExternal({
-				packagePath: join(process.cwd(), 'package.json'),
-			}),
 			dts(),
+			nodeResolve()
 		],
 	},
 ]);
